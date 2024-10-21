daa7acced2935ec205932f5f5314eba6658c943f
1 <test@tut.by>
2024-10-21 21:56:29 +0300

    commit

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -4,11 +4,10 @@ public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
         int thousands = number / 1000;
-        int hundreds = number / 100;
+        int hundreds = (number - thousands*1000) / 100;
         int tens = (number / 10) % 10;
         int ones = number % 10;
 
         System.out.println(thousands + hundreds + tens + ones);
     }
-
 }
